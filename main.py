31e9b98e9bbaa1d7cf1ffa07c8f9946c1ceea2a8
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-07-20 14:52:45 +0300

    flask support added

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
 import cv2
 import cvzone
 
-thres = 0.55
-nmsThres = 0.2
-cap = cv2.VideoCapture(1)
-# cap.set(3, 640)
-# cap.set(4, 480)
+import time
 
-classNames = []
-classFile = 'coco.names'
-with open(classFile, 'rt') as f:
-    classNames = f.read().split('\n')
-print(classNames)
-configPath = 'model_mobilenet.pbtxt'
-weightsPath = "frozen_inference_graph.pb"
+import cv2
+from flask import Flask, render_template, Response
 
-net = cv2.dnn_DetectionModel(weightsPath, configPath)
-net.setInputSize(320, 320)
-net.setInputScale(1.0 / 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
-
-while True:
-    success, img = cap.read()
-    classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nmsThres)
-    try:
-        for classId, conf, box in zip(classIds.flatten(), confs.flatten(), bbox):
-            cvzone.cornerRect(img, box)
-            cv2.putText(img, f'{classNames[classId - 1].upper()} {round(conf * 100, 2)}',
-                        (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                        1, (0, 255, 0), 2)
-    except:
-        pass
+app = Flask(__name__)
 
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+def gen():
+    thres = 0.55
+    nmsThres = 0.2
+    cap = cv2.VideoCapture(0)
+    # cap.set(3, 640)
+    # cap.set(4, 480)
+
+    classNames = []
+    classFile = 'coco.names'
+    with open(classFile, 'rt') as f:
+        classNames = f.read().split('\n')
+    print(classNames)
+    configPath = 'model_mobilenet.pbtxt'
+    weightsPath = "frozen_inference_graph.pb"
+
+    net = cv2.dnn_DetectionModel(weightsPath, configPath)
+    net.setInputSize(320, 320)
+    net.setInputScale(1.0 / 127.5)
+    net.setInputMean((127.5, 127.5, 127.5))
+    net.setInputSwapRB(True)
+
+    while True:
+        success, img = cap.read()
+        classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nmsThres)
+        try:
+            for classId, conf, box in zip(classIds.flatten(), confs.flatten(), bbox):
+                cvzone.cornerRect(img, box)
+                cv2.putText(img, f'{classNames[classId - 1].upper()} {round(conf * 100, 2)}',
+                            (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                            1, (0, 255, 0), 2)
+        except Exception as e:
+            print(e)
+        success, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
