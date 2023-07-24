d3ae7580daaa4649411e2299b74627ceb687a5e2
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-07-24 14:19:53 +0300

    pans

--- servo.py
+++ servo.py
@@ -1,16 +1,16 @@
 import serial
 
 #НОМЕР COM ПОРТА
-# ser = serial.Serial('COM6', 1200)
-# ser.close()
+ser = serial.Serial('COM6', 1200)
+ser.close()
 
 image_width = 640
 image_height = 480
 
 def send_pelco_d_command(command):
-    # ser.open()
-    # ser.write(command)
-    print(command)
+    ser.open()
+    ser.write(command)
+    # print(command)
 
 def move_camera_to_target(target_center):
     # Calculate the center of the image
