24f8d7b2817bee532481904d5bcc56c6a1482aca
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-07-26 13:43:27 +0300

    new logic

--- main.py
+++ main.py
@@ -105,6 +105,7 @@ def video_feed():
 
 @app.route("/exit")
 def exit_app():
+    ser.write(bytearray([0xFF, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01]))
     global exiting
     exiting = True
     return "Done"
@@ -117,7 +118,6 @@ def teardown(exception):
             cap.release()
         except:
             pass
-        ser.write(bytearray([0xFF, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01]))
         os._exit(0)
 
 
